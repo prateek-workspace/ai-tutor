23ff531afd93cec4af1e681b9588b3d80bf7e24f
Prateek Srivastava <workspace.infoga@gmail.com>
2026-06-18 01:26:00 +0530

    v1.2

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      // Cosmetic rules that should not fail the production build.
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
 
 export default eslintConfig;
